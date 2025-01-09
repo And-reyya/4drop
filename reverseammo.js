b6e29297de3004d3bf9b85ea90322cdc99daa063
And-reyya <nail.atw@gmail.com>
2025-01-09 01:10:29 +0100

    reverseammo.js

--- reverseammo.js
+++ reverseammo.js
@@ -1,126 +1,147 @@
-function showReversePont(e) {
-    fetch("https://raw.githubusercontent.com/And-reyya/scumtools/main/reversetools.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok " + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log("reversepont data:", data);
-            const ammoData = data.reversepont;
-            if (!ammoData) throw new Error("reversepont is undefined");
-            const reversePont = document.createElement("div");
-            reversePont.id = "reversepont";
-            reversePont.classList.add("dropdown");
+document.addEventListener("DOMContentLoaded", () => {
+    let e = document.getElementById("searchInput"),
+        t = document.getElementById("reversedrop"),
+        n = document.createElement("div");
+    n.id = "reversemmagazine";
+    n.classList.add("dropdown");
+    document.querySelector(".search-container").appendChild(n);
 
-            ammoData.forEach(item => {
-                let p = document.createElement("p");
-                p.textContent = item.standard;
-                p.addEventListener("click", () => {
-                    e.value = `${item.standard}`;
-                    reversePont.style.display = "none";
-                    showReverseMarika(e);
-                });
-                reversePont.appendChild(p);
+    let o = null;
+
+    function c(t, n) {
+        let l = document.createElement("div");
+        l.id = "reversemmagazine2";
+        l.classList.add("dropdown");
+        l.innerHTML = "";
+        t.forEach(t => {
+            let a = document.createElement("p");
+            a.textContent = t;
+            a.addEventListener("click", () => {
+                e.value = `${n}; ${t}`;
+                l.style.display = "none";
+                o = null;
             });
-
-            document.querySelector(".search-container").appendChild(reversePont);
-            reversePont.style.display = "block";
-        })
-        .catch(error => console.error("Error fetching reversepont data: ", error));
-}
-
-function showReverseMarika(e) {
-    const reverseMarika = document.createElement("div");
-    reverseMarika.id = "reversemarika";
-    reverseMarika.classList.add("dropdown");
-
-    ["WEAPON", "MAGAZINE"].forEach(item => {
-        let p = document.createElement("p");
-        p.textContent = item;
-        p.addEventListener("click", () => {
-            e.value += `; ${item}`;
-            reverseMarika.style.display = "none";
-            if (item === "WEAPON") {
-                showReverseJancsi(e);
-            } else if (item === "MAGAZINE") {
-                showReverseMonika(e);
-            }
+            l.appendChild(a);
         });
-        reverseMarika.appendChild(p);
+        document.querySelector(".search-container").appendChild(l);
+        l.style.display = "block";
+        return l;
+    }
+
+    // AMMO kiválasztása esetén új dropdown
+    function showReversePont() {
+        fetch("https://raw.githubusercontent.com/And-reyya/scumtools/main/reversetools.json")
+            .then(response => response.json())
+            .then(data => {
+                const ammoData = data.reversepont;
+                const reversePont = document.createElement("div");
+                reversePont.id = "reversepont";
+                reversePont.classList.add("dropdown");
+
+                ammoData.forEach(item => {
+                    let p = document.createElement("p");
+                    p.textContent = item.standard;
+                    p.addEventListener("click", () => {
+                        e.value = `${item.standard}`;
+                        reversePont.style.display = "none";
+                        showReverseMarika();
+                    });
+                    reversePont.appendChild(p);
+                });
+
+                document.querySelector(".search-container").appendChild(reversePont);
+                reversePont.style.display = "block";
+            });
+    }
+
+    // AMMO kiválasztása
+    t.addEventListener("click", async m => {
+        if ("P" === m.target.tagName) {
+            if ("AMMO" === m.target.textContent) {
+                t.style.display = "none";
+                showReversePont();
+            }
+        }
     });
 
-    document.querySelector(".search-container").appendChild(reverseMarika);
-    reverseMarika.style.display = "block";
-}
+    // Új dropdown reversemarika
+    function showReverseMarika() {
+        const reverseMarika = document.createElement("div");
+        reverseMarika.id = "reversemarika";
+        reverseMarika.classList.add("dropdown");
 
-function showReverseJancsi(e) {
-    fetch("https://raw.githubusercontent.com/And-reyya/scumtools/main/reversetools.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok " + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log("reversejancsi data:", data);
-            const jancsiData = data.reversejancsi;
-            if (!jancsiData) throw new Error("reversejancsi is undefined");
-            const reverseJancsi = document.createElement("div");
-            reverseJancsi.id = "reversejancsi";
-            reverseJancsi.classList.add("dropdown");
-
-            let selectedAmmo = e.value.split(";")[0].trim();
-            jancsiData.filter(item => item.ammo === selectedAmmo).forEach(item => {
-                item.weapon.forEach(weapon => {
-                    let p = document.createElement("p");
-                    p.textContent = weapon;
-                    p.addEventListener("click", () => {
-                        e.value += `; ${weapon}`;
-                        reverseJancsi.style.display = "none";
-                    });
-                    reverseJancsi.appendChild(p);
-                });
+        ["WEAPON", "MAGAZINE"].forEach(item => {
+            let p = document.createElement("p");
+            p.textContent = item;
+            p.addEventListener("click", () => {
+                e.value += `; ${item}`;
+                reverseMarika.style.display = "none";
+                if (item === "WEAPON") {
+                    showReverseJancsi();
+                } else if (item === "MAGAZINE") {
+                    showReverseMonika();
+                }
             });
+            reverseMarika.appendChild(p);
+        });
 
-            document.querySelector(".search-container").appendChild(reverseJancsi);
-            reverseJancsi.style.display = "block";
-        })
-        .catch(error => console.error("Error fetching reversejancsi data: ", error));
-}
+        document.querySelector(".search-container").appendChild(reverseMarika);
+        reverseMarika.style.display = "block";
+    }
 
-function showReverseMonika(e) {
-    fetch("https://raw.githubusercontent.com/And-reyya/scumtools/main/reversetools.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response was not ok " + response.statusText);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log("reversemonika data:", data);
-            const monikaData = data.reversemonika;
-            if (!monikaData) throw new Error("reversemonika is undefined");
-            const reverseMonika = document.createElement("div");
-            reverseMonika.id = "reversemonika";
-            reverseMonika.classList.add("dropdown");
+    // Új dropdown reversejancsi
+    function showReverseJancsi() {
+        fetch("https://raw.githubusercontent.com/And-reyya/scumtools/main/reversetools.json")
+            .then(response => response.json())
+            .then(data => {
+                const jancsiData = data.reversejancsi;
+                const reverseJancsi = document.createElement("div");
+                reverseJancsi.id = "reversejancsi";
+                reverseJancsi.classList.add("dropdown");
 
-            let selectedAmmo = e.value.split(";")[0].trim();
-            monikaData.filter(item => item.ammo === selectedAmmo).forEach(item => {
-                item.magazine.forEach(magazine => {
-                    let p = document.createElement("p");
-                    p.textContent = magazine;
-                    p.addEventListener("click", () => {
-                        e.value += `; ${magazine}`;
-                        reverseMonika.style.display = "none";
+                let selectedAmmo = e.value.split(";")[0].trim();
+                jancsiData.filter(item => item.ammo === selectedAmmo).forEach(item => {
+                    item.weapon.forEach(weapon => {
+                        let p = document.createElement("p");
+                        p.textContent = weapon;
+                        p.addEventListener("click", () => {
+                            e.value += `; ${weapon}`;
+                            reverseJancsi.style.display = "none";
+                        });
+                        reverseJancsi.appendChild(p);
                     });
-                    reverseMonika.appendChild(p);
                 });
-            });
 
-            document.querySelector(".search-container").appendChild(reverseMonika);
-            reverseMonika.style.display = "block";
-        })
-        .catch(error => console.error("Error fetching reversemonika data: ", error));
-}
+                document.querySelector(".search-container").appendChild(reverseJancsi);
+                reverseJancsi.style.display = "block";
+            });
+    }
+
+    // Új dropdown reversemonika
+    function showReverseMonika() {
+        fetch("https://raw.githubusercontent.com/And-reyya/scumtools/main/reversetools.json")
+            .then(response => response.json())
+            .then(data => {
+                const monikaData = data.reversemonika;
+                const reverseMonika = document.createElement("div");
+                reverseMonika.id = "reversemonika";
+                reverseMonika.classList.add("dropdown");
+
+                let selectedAmmo = e.value.split(";")[0].trim();
+                monikaData.filter(item => item.ammo === selectedAmmo).forEach(item => {
+                    item.magazine.forEach(magazine => {
+                        let p = document.createElement("p");
+                        p.textContent = magazine;
+                        p.addEventListener("click", () => {
+                            e.value += `; ${magazine}`;
+                            reverseMonika.style.display = "none";
+                        });
+                        reverseMonika.appendChild(p);
+                    });
+                });
+
+                document.querySelector(".search-container").appendChild(reverseMonika);
+                reverseMonika.style.display = "block";
+            });
+    }
+});
